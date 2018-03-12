5b18fa83cb926bdbd13568ecca0f7cd96fedd78e
Byungwook Lee <byungwook.lee@nhn.com>
2018-03-12 16:26:34 +0900

    23.10 Admin Features
    - jconsole 로 보기
    1. jconsole cmd 에서 입력
    2. application 보기
    3. application shutdown 가능 -> MBeans > org.springframework.boot > Admin > SpringApplication > Operations > shutdown

--- src/main/java/me/spring/boot/user/UserService.java
+++ src/main/java/me/spring/boot/user/UserService.java
@@ -1,10 +1,6 @@
 package me.spring.boot.user;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Service;
 
 /**
